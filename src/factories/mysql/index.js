92769a56095c867c1c6e165ff97ab27dbd8338a2
Leo Lima <leonardotlima2@gmail.com>
2020-06-23 19:18:47 -0300

    Fix indent and package.json

--- src/factories/mysql/index.js
+++ src/factories/mysql/index.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const mysql = new Sequelize(
+const sequelize = new Sequelize(
     process.env.DB_MYSQL_DATABASE,
     process.env.DB_MYSQL_USERNAME, 
     process.env.DB_MYSQL_PASSWORD, 
@@ -10,14 +10,14 @@ const mysql = new Sequelize(
         port: process.env.DB_MYSQL_PORT,
         pool: {
             max: 5,
-            min: 0,
+            min: 2,
             acquire: 30000,
             idle: 10000
         }
     }
 );
 
-mysql
+sequelize
   .authenticate()
   .then(() => {
     console.log('✅ MySQL - Connection has been established successfully.');
@@ -26,4 +26,4 @@ mysql
     console.error('MySQL - Unable to connect to the database:', err);
   });
 
-module.exports = mysql;  
+module.exports = sequelize;  
